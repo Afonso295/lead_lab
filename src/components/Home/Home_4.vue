a40c8082631a6fed0f93f844ad007c9f5999cd18
Afonso295 <152220916+Afonso295@users.noreply.github.com>
2025-06-11 15:22:10 +0100

    update geral no projeto

--- src/components/Home/Home_4.vue
+++ src/components/Home/Home_4.vue
@@ -1,11 +1,15 @@
 <template>
-  <div classe="txt">
-    <span>O seu Marketing.<br />O nosso compromisso.</span>
-    <span>
-      Assumimos a Gestão do seu departamento de Marketing, ou tornamo-nos a
-      extensão perfeita para as suas equipas de Marketing, Recursos Humanos ou
-      qualquer outra unidade de negócio que queira crescer no digital.
-    </span>
+  <div class="home">
+    <div class="txt1">
+      <span>O seu Marketing.<br />O nosso compromisso.<br /></span>
+    </div>
+    <div class="txt2">
+      <span>
+        Assumimos a Gestão do seu departamento de Marketing, ou tornamo-nos a
+        extensão perfeita para as suas equipas de Marketing, Recursos Humanos ou
+        qualquer outra unidade de negócio que queira crescer no digital.
+      </span>
+    </div>
   </div>
 </template>
 
@@ -16,9 +20,18 @@ export default {
 </script>
 
 <style scoped>
-.txt {
+.home {
   color: aliceblue;
+  text-align: center;
+  padding: 1px;
+  margin-top: 200px;
+}
+.txt1 {
   font-weight: bold;
   font-size: 59px;
+}
+.txt2 {
+  font-weight: bold;
+  font-size: 23px;
 }
 </style>
