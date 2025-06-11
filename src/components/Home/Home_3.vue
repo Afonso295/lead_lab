c4090edbd3c5da3493e8523bb5bc276397c817b2
Afonso295 <152220916+Afonso295@users.noreply.github.com>
2025-06-11 14:38:06 +0100

    Update Home_3.vue

--- src/components/Home/Home_3.vue
+++ src/components/Home/Home_3.vue
@@ -1,72 +1,77 @@
 <template>
   <div class="home">
-    <div class="txt">
-      <span class="txt1">A nossa metodologia.</span>
-      <span class="txt2">
-        <br />Conheça a nossa abordagem estratégica para resultados de
-        excelência.
-      </span>
+    <div class="col-esquerda">
+      <div class="txt">
+        <span class="txt1">A nossa metodologia.</span>
+        <span class="txt2">
+          <br />Conheça a nossa abordagem estratégica para resultados de
+          excelência.
+        </span>
+      </div>
     </div>
-    <div class="tabela">
-      <div class="linha">
-        <ul>
-          <li><span class="txt_linha"> 1. DISCOVERY</span></li>
-          <li>
-            <span class="txt1_linha"
-              >Primeira reunião com um levantamento de necessidades do cliente,
-              identificando objetivos.</span
-            >
-          </li>
-        </ul>
-        <ul>
-          <li><span class="txt_linha">2. BLUEPRINT</span></li>
-          <li>
-            <span class="txt1_linha"
-              >Primeira reunião com um levantamento de necessidades do cliente,
-              identificando objetivos.</span
-            >
-          </li>
-        </ul>
-      </div>
-      <div class="linha">
-        <ul>
-          <li><span class="txt_linha">3. EXECUTION</span></li>
-          <li>
-            <span class="txt1_linha">
-              Primeira reunião com um levantamento de necessidades do cliente,
-              identificando objetivos.
-            </span>
-          </li>
-        </ul>
-        <ul>
-          <li><span class="txt_linha">4. DELIGHT</span></li>
-          <li>
-            <span class="txt1_linha">
-              Primeira reunião com um levantamento de necessidades do cliente,
-              identificando objetivos.
-            </span>
-          </li>
-        </ul>
-      </div>
-      <div class="linha">
-        <ul>
-          <li><span class="txt_linha">5. ANALYTICS</span></li>
-          <li>
-            <span class="txt1_linha"
-              >Primeira reunião com um levantamento de necessidades do cliente,
-              identificando objetivos.</span
-            >
-          </li>
-        </ul>
-        <ul>
-          <li><span class="txt_linha">6. OPTIMIZATION</span></li>
-          <li>
-            <span class="txt1_linha"
-              >Primeira reunião com um levantamento de necessidades do cliente,
-              identificando objetivos.</span
-            >
-          </li>
-        </ul>
+
+    <div class="col-direita">
+      <div class="tabela">
+        <div class="linha">
+          <ul>
+            <li><span class="txt_linha">1. DISCOVERY</span></li>
+            <li>
+              <span class="txt1_linha"
+                >Primeira reunião com um levantamento de necessidades do
+                cliente, identificando objetivos.</span
+              >
+            </li>
+          </ul>
+          <ul>
+            <li><span class="txt_linha">2. BLUEPRINT</span></li>
+            <li>
+              <span class="txt1_linha"
+                >Primeira reunião com um levantamento de necessidades do
+                cliente, identificando objetivos.</span
+              >
+            </li>
+          </ul>
+        </div>
+        <div class="linha">
+          <ul>
+            <li><span class="txt_linha">3. EXECUTION</span></li>
+            <li>
+              <span class="txt1_linha"
+                >Primeira reunião com um levantamento de necessidades do
+                cliente, identificando objetivos.</span
+              >
+            </li>
+          </ul>
+          <ul>
+            <li><span class="txt_linha">4. DELIGHT</span></li>
+            <li>
+              <span class="txt1_linha"
+                >Primeira reunião com um levantamento de necessidades do
+                cliente, identificando objetivos.</span
+              >
+            </li>
+          </ul>
+        </div>
+        <div class="linha">
+          <ul>
+            <li><span class="txt_linha">5. ANALYTICS</span></li>
+            <li>
+              <span class="txt1_linha"
+                >Primeira reunião com um levantamento de necessidades do
+                cliente, identificando objetivos.</span
+              >
+            </li>
+          </ul>
+          <ul>
+            <li><span class="txt_linha">6. OPTIMIZATION</span></li>
+            <li>
+              <span class="txt1_linha"
+                >Primeira reunião com um levantamento de necessidades do
+                cliente, identificando objetivos.</span
+              >
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -80,56 +85,65 @@ export default {
 
 <style scoped>
 .home {
-  color: aliceblue;
-  padding: 1px;
   display: flex;
-  flex-direction: row;
-  align-items: flex-start;
-  flex-wrap: wrap;
-  margin-bottom: 30px;
+  gap: 40px;
+  color: aliceblue;
+  padding: 10px;
+  align-items: center;
+  flex-wrap: wrap; /* Para ficar responsivo em ecrãs pequenos */
+  min-height: 100vh; /* ocupar a altura toda da viewport */
+}
+
+/* Coluna da esquerda - texto principal */
+.col-esquerda {
+  flex: 1 1 400px;
+  max-width: 500px;
+  justify-content: center;
+}
+
+/* Coluna da direita - tabela */
+.col-direita {
+  flex: 2 1 600px; /* ocupa mais espaço */
+  max-width: 900px;
 }
 
 .txt1 {
-  margin-top: 100px;
   font-size: 59px;
   font-weight: bold;
-  text-align: center;
-  max-width: 1000px;
+  text-align: left;
+  max-width: 100%;
+  line-height: 70px;
 }
 
 .txt2 {
-  margin-top: 20px;
   font-size: 23px;
   font-weight: bold;
-  text-align: center;
-  max-width: 1050px;
+  text-align: left;
+  max-width: 100%;
 }
+
 .txt {
-  max-width: 500px;
-  margin-left: 10px;
-}
-.txt_linha {
-  font-size: 20px;
-}
-.txt1_linha {
-  font-size: 14px;
+  margin-left: 60px;
+  max-width: 400px;
+  color: aliceblue;
+  margin-top: 10px;
 }
 
 .tabela {
   display: flex;
   flex-direction: column;
   gap: 15px;
-  margin-top: 40px;
-  margin-left: auto;
-  margin-right: 10px;
+  margin-top: 30px;
+  margin-left: 0;
+  margin-right: 0;
 }
 
 .linha {
   display: flex;
   gap: 20px;
-  justify-content: center;
+  justify-content: flex-start;
   max-width: 900px;
-  margin-bottom: -35px;
+  margin-bottom: 20px;
 }
 
 .tabela ul {
@@ -138,5 +152,15 @@ export default {
   border: 1px solid rgb(47, 47, 47);
   list-style: none;
   font-size: 20px;
+  flex: 1;
+}
+
+.txt_linha {
+  font-size: 20px;
+  color: rgb(0, 255, 136);
+}
+
+.txt1_linha {
+  font-size: 14px;
 }
 </style>
